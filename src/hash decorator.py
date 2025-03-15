9427c7ac666f80c6841e806588bc34a2cdd81d3d
Dmitry Moskalev <moskalev199618120@gmail.com>
2025-03-15 14:59:20 +0300

    done

---
+++ src/hash decorator.py
@@ -0,0 +1,47 @@
+import functools
+import unittest.mock
+
+
+def lru_cache(*args, **kwargs):
+    def decorator(func):
+        cache = functools.lru_cache(*args, **kwargs)(func)
+        return cache
+    return decorator
+
+
+@lru_cache()
+def summ(a: int, b: int) -> int:
+    return a + b
+
+
+@lru_cache()
+def sum_many(a: int, b: int, *, c: int, d: int) -> int:
+    return a + b + c + d
+
+
+@lru_cache(maxsize=3)
+def multiply(a: int, b: int) -> int:
+    return a * b
+
+
+if __name__ == '__main__':
+    assert summ(1, 2) == 3
+    assert summ(3, 4) == 7
+
+    assert multiply(1, 2) == 2
+    assert multiply(3, 4) == 12
+
+    assert sum_many(1, 2, c=3, d=4) == 10
+
+    mocked_func = unittest.mock.Mock()
+    mocked_func.side_effect = [1, 2, 3, 4]
+
+    decorated = lru_cache(maxsize=2)(mocked_func)
+    assert decorated(1, 2) == 1
+    assert decorated(1, 2) == 1
+    assert decorated(3, 4) == 2
+    assert decorated(3, 4) == 2
+    assert decorated(5, 6) == 3
+    assert decorated(5, 6) == 3
+    assert decorated(1, 2) == 4
+    assert mocked_func.call_count == 4
